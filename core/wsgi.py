84ca20eeb8d33bebf8f40002314d2c3a707a426f
Sodiqjon <murodovsodiq1800@gmail.com>
2024-10-28 16:32:06 +0500

    add static files

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Adjust the paths below
+path = '/home/Sodiqjon/restaurant-booking-system'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
+
+# Activate the virtual environment
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
