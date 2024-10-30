d15d7ff5adcb33c5fbbcc6f16ae7fc4be1a8cbcc
Sodiqjon <murodovsodiq1800@gmail.com>
2024-10-30 16:29:27 +0500

    fix deploy

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,13 +1,6 @@
 import os
-import sys
-
-# Adjust the paths below
-path = '/home/Sodiqjon/restaurant-booking-system'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
-
-# Activate the virtual environment
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 application = get_wsgi_application()
